4dac1c22c724b8854a471d039709a98480c32bd7
holubyulia <88442295+holubyulia@users.noreply.github.com>
2021-08-20 21:50:34 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     var currentFloor = 2; /* переменная где хранится текущий этаж */
     var floorPath = $(".home_img path"); /* отдельный этаж в SVG*/
-    var counterUp = $(".counter - up"); /* кнопка увеличения этажа */ 
-    var counterDown = $(".counter - down"); /* кнопка уменьшения этажа */
+    var counterUp = $(".counter-up"); /* кнопка увеличения этажа */ 
+    var counterDown = $(".counter-down"); /* кнопка уменьшения этажа */
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
     
     //функция при наведении мышкой на этаж
     floorPath.on('mouseover', function () {
@@ -11,14 +14,19 @@ $(document).ready(function () {
      $(".counter").text(currentFloor); // записываем значение этажа в счетчик
     });
 
+    floorPath.on("click", toggleModal);
+    modalCloseButton.on("click", toggleModal);
+    viewFlatsButton.on("click", toggleModal);
+
+
     counterUp.on("click", function () { //отслеживаем клик по кнопке вверх
         if (currentFloor < 18) { //проверяем значение этажа
             currentFloor++; // прибавляем 1 этаж
             usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2,
             useGrouping: false }); // форматируем переменную с этажом чтобы было не 2, а 02
             $(".counter").text(usCurrentFloor); // записываем значение этажа в счетчик
-            floorPath.removeClass('current-floor'); //удаляем активный клас этажей
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); //подсвечиваем текущий этаж
+            floorPath.removeClass("current-floor"); //удаляем активный клас этажей
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); //подсвечиваем текущий этаж
         }
     });
 
@@ -27,8 +35,12 @@ $(document).ready(function () {
             currentFloor--;
             usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false });
             $(".counter").text(usCurrentFloor);
-            floorPath.removeClass('current-floor');
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+            floorPath.removeClass("current-floor");
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
     });
+    function toggleModal() {
+        modal.toggleClass("is.open");
+     }
+
 });
